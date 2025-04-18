f4eb11082a0f81e52bafdfcd256c89c075cfa087
Rodrigo <rodrigosnr@hotmail.com>
2025-04-18 01:38:57 -0300

    feat: implementa métodos de update e delete e validações dos campos

--- src/main/java/com/devrodrigosnr/crud_spring/controller/CourseController.java
+++ src/main/java/com/devrodrigosnr/crud_spring/controller/CourseController.java
@@ -3,13 +3,16 @@ package com.devrodrigosnr.crud_spring.controller;
 import com.devrodrigosnr.crud_spring.model.Course;
 import com.devrodrigosnr.crud_spring.repository.CourseRepository;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +32,7 @@ public class CourseController {
     }
 
     @PostMapping
-    public ResponseEntity<Course> create(@RequestBody Course course){
+    public ResponseEntity<Course> create(@RequestBody @Valid Course course){
         return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
     }
 
@@ -40,4 +43,27 @@ public class CourseController {
                 .orElse(ResponseEntity.notFound().build());
 
 	}
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> update(@PathVariable Long id, 
+        @RequestBody @Valid Course course) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    Course updated = courseRepository.save(recordFound);
+                    return ResponseEntity.ok().body(updated);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        return courseRepository.findById(id)
+                .map(recordFound -> {
+                    courseRepository.deleteById(id);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/devrodrigosnr/crud_spring/model/Course.java
+++ src/main/java/com/devrodrigosnr/crud_spring/model/Course.java
@@ -1,19 +1,38 @@
 package com.devrodrigosnr.crud_spring.model;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Column(length = 100, nullable = false)
     private String name;
 
-    @Column(length = 100, nullable = false)
+    @NotBlank
+    @NotNull
+    @Pattern(regexp = "Backend|Frontend")
+    @Column(length = 10, nullable = false)
     private String category;
+
+    @NotBlank
+    @NotNull
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 }
