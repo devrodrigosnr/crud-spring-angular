b48d5bffd83d087a9bf2bd3dec2bb9b2bd45233d
Rodrigo <rodrigosnr@hotmail.com>
2025-04-18 20:37:05 -0300

    refactor: migração dos métodos do controller para a classe de serviço

--- src/main/java/com/devrodrigosnr/crud_spring/controller/CourseController.java
+++ src/main/java/com/devrodrigosnr/crud_spring/controller/CourseController.java
@@ -1,10 +1,9 @@
 package com.devrodrigosnr.crud_spring.controller;
 
 import com.devrodrigosnr.crud_spring.model.Course;
-import com.devrodrigosnr.crud_spring.repository.CourseRepository;
+import com.devrodrigosnr.crud_spring.service.CourseService;
 
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,24 +20,27 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor
 public class CourseController {
 
-    private final CourseRepository courseRepository;
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping
     public List<Course> list(){
-        return courseRepository.findAll();
+        return courseService.list();
     }
 
     @PostMapping
     public ResponseEntity<Course> create(@RequestBody @Valid Course course){
-        return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.create(course));
     }
 
     @GetMapping("/{id}")
 	public ResponseEntity<Course> findById(@PathVariable Long id) {
-		return courseRepository.findById(id)
+		return courseService.findById(id)
                 .map(course -> ResponseEntity.ok(course))
                 .orElse(ResponseEntity.notFound().build());
 
@@ -47,23 +49,16 @@ public class CourseController {
     @PutMapping("/{id}")
     public ResponseEntity<Course> update(@PathVariable Long id, 
         @RequestBody @Valid Course course) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    Course updated = courseRepository.save(recordFound);
-                    return ResponseEntity.ok().body(updated);
-                })
-                .orElse(ResponseEntity.notFound().build());
+        return courseService.update(id, course)
+        .map(recordFound -> ResponseEntity.ok().body(recordFound))
+        .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return ResponseEntity.noContent().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
+        if(courseService.delete(id)){
+            return ResponseEntity.noContent().<Void>build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/devrodrigosnr/crud_spring/service/CourseService.java
+++ src/main/java/com/devrodrigosnr/crud_spring/service/CourseService.java
@@ -3,9 +3,12 @@ package com.devrodrigosnr.crud_spring.service;
 import com.devrodrigosnr.crud_spring.model.Course;
 import com.devrodrigosnr.crud_spring.repository.CourseRepository;
 
+import jakarta.validation.Valid;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseService {
@@ -16,7 +19,33 @@ public class CourseService {
         this.repository = repository;
     }
 
-    public List<Course> buscarTodosOsCursos(){
+    public List<Course> list(){
         return this.repository.findAll();
+    }
+
+    public Course create(@Valid Course course){
+        return repository.save(course);
+    }
+
+    public Optional<Course> findById(Long id) {
+        return repository.findById(id);
+	}
+
+    public Optional<Course> update(Long id, @Valid Course course) {
+        return repository.findById(id)
+                .map(recordFound -> {
+                    recordFound.setName(course.getName());
+                    recordFound.setCategory(course.getCategory());
+                    return repository.save(recordFound);
+                });
+    }
+
+    public boolean delete(Long id) {
+    return repository.findById(id)
+            .map(recordFound -> {
+                repository.deleteById(id);
+                return true;
+            })
+            .orElse(false);
     }
 }
