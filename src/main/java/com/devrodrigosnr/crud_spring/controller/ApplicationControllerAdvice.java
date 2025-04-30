d21acf1eb6be446dc9912dde020ea4f1b1169efc
Rodrigo <rodrigosnr@hotmail.com>
2025-04-30 01:13:00 -0300

    feat: inclusão de tratamento de exceção

---
+++ src/main/java/com/devrodrigosnr/crud_spring/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,18 @@
+package com.devrodrigosnr.crud_spring.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+
+import com.devrodrigosnr.crud_spring.exception.RecordNotFoundException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNotFoundException(RecordNotFoundException e) {
+        return e.getMessage();
+    }
+}
--- src/main/java/com/devrodrigosnr/crud_spring/controller/CourseController.java
+++ src/main/java/com/devrodrigosnr/crud_spring/controller/CourseController.java
@@ -1,25 +1,28 @@
 package com.devrodrigosnr.crud_spring.controller;
 
-import com.devrodrigosnr.crud_spring.model.Course;
+import com.devrodrigosnr.crud_spring.dto.CourseDTO;
 import com.devrodrigosnr.crud_spring.service.CourseService;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
+@Validated
 public class CourseController {
 
     private final CourseService courseService;
@@ -29,36 +32,30 @@ public class CourseController {
     }
 
     @GetMapping
-    public List<Course> list(){
+    public List<CourseDTO> list(){
         return courseService.list();
     }
 
     @PostMapping
-    public ResponseEntity<Course> create(@RequestBody @Valid Course course){
-        return ResponseEntity.status(HttpStatus.CREATED).body(courseService.create(course));
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public CourseDTO create(@RequestBody @NotNull @Valid CourseDTO course){
+        return courseService.create(course);
     }
 
     @GetMapping("/{id}")
-	public ResponseEntity<Course> findById(@PathVariable Long id) {
-		return courseService.findById(id)
-                .map(course -> ResponseEntity.ok(course))
-                .orElse(ResponseEntity.notFound().build());
-
+	public CourseDTO findById(@PathVariable Long id) {
+		return courseService.findById(id);
 	}
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update(@PathVariable Long id, 
-        @RequestBody @Valid Course course) {
-        return courseService.update(id, course)
-        .map(recordFound -> ResponseEntity.ok().body(recordFound))
-        .orElse(ResponseEntity.notFound().build());
+    public CourseDTO update(@PathVariable @NotNull Long id, 
+        @RequestBody @Valid CourseDTO course) {
+        return courseService.update(id, course);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        if(courseService.delete(id)){
-            return ResponseEntity.noContent().<Void>build();
-        }
-        return ResponseEntity.notFound().build();
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        courseService.delete(id);
     }
 }
