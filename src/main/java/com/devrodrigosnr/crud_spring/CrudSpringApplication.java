2242f29b67f95e1f538a0d9014b5cff0a41853de
Rodrigo <dev.rodrigosnr@gmail.com>
2025-04-30 01:29:52 -0300

    refactor: dados de teste

--- src/main/java/com/devrodrigosnr/crud_spring/CrudSpringApplication.java
+++ src/main/java/com/devrodrigosnr/crud_spring/CrudSpringApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.devrodrigosnr.crud_spring.enums.Category;
 import com.devrodrigosnr.crud_spring.model.Course;
+import com.devrodrigosnr.crud_spring.model.Lesson;
 import com.devrodrigosnr.crud_spring.repository.CourseRepository;
 
 @SpringBootApplication
@@ -22,13 +24,25 @@ public class CrudSpringApplication {
 
             Course c = new Course();
             c.setName("Spring Boot");
-            c.setCategory("Backend");
+            c.setCategory(Category.BACKEND);
 
             repository.save(c);
 
 			Course c1 = new Course();
             c1.setName("Angular");
-            c1.setCategory("Frontend");
+            c1.setCategory(Category.FRONTEND);
+
+            Lesson l = new Lesson();
+            l.setName("Introdução");
+            l.setYoutubeURL("watch.?aasas");
+            l.setCourse(c1);
+            c1.getLessons().add(l);
+
+            Lesson l1 = new Lesson();
+            l1.setName("Introdução");
+            l1.setYoutubeURL("watch.?aasas");
+            l1.setCourse(c1);
+            c1.getLessons().add(l1);
 
             repository.save(c1);
         };
