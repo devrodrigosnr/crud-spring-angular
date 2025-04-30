62c2ee1082d5a9dfc215b69111b381b07680a2b5
Rodrigo <rodrigosnr@hotmail.com>
2025-04-30 01:04:52 -0300

    feat: inclui dialog component

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -16,12 +16,31 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-      delay(5000),
+      delay(1000),
       tap(courses => console.log(courses))
     );
   }
 
   save(record: Partial<Course>){
-    return this.httpClient.post<Course>(this.API, record)
+    if(record.id) {
+      return this.update(record);
+    }
+    return this.create(record);
+  }
+
+  loadById(id: string){
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
+  private create(record: Partial<Course>){
+    return this.httpClient.post<Course>(this.API, record);
+  }
+
+  private update(record: Partial<Course>){
+    return this.httpClient.put<Course>(`${this.API}/${record.id}`, record);
+  }
+
+  delete(id: string) {
+    return this.httpClient.delete(`${this.API}/${id}`);
   }
 }
